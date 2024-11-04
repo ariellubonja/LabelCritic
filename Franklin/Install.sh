8afbd279e0f4b1651b832bdac19c8c4f122226e6
Pedro Ricardo Ariel Salvador Bassi <39171826+PedroRASB@users.noreply.github.com>
2024-11-04 11:46:48 -0500

    Delete Franklin directory

--- Franklin/Install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#PBS -l select=1:ncpus=20:ngpus=0
-#PBS -l walltime=23:59:00
-#PBS -j oe
-#PBS -N AbdomenAtlas11Download
-#PBS -q cpu
-
-cd /fastwork/psalvador/JHU/AnnotationVLM
-bash InstallEnv.sh > installation.log 2>&1
--- Franklin/InstallLlama.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#PBS -l select=1:ncpus=20:ngpus=0
-#PBS -l walltime=23:59:00
-#PBS -j oe
-#PBS -N llamaInstall
-#PBS -q cpu
-
-cd /fastwork/psalvador/JHU/AnnotationVLM
-bash InstallEnvLlama.sh > installation.log 2>&1
